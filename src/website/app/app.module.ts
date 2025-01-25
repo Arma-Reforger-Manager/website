bf4dceecf0d43b6fb8d36e7a90331e8001d28ae9
+Flabby <plusflabby@gmail.com>
2025-01-25 08:17:23 -0500

    Examples

--- src/website/app/app.module.ts
+++ src/website/app/app.module.ts
@@ -1,18 +1,36 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// import { AppComponent as App2 } from './app.component';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: false
+})
+export class AppComponent {
+  title = 'zomato-clone';
+}
 
 @NgModule({
   declarations: [
-    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule, // add here if necessasry
+    RouterLinkActive,// add here if necessasry
+    RouterOutlet, // add here if necessasry
+    RouterLink,// add here if necessasry
+    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, LoginComponent]
 })
 export class AppModule { }
